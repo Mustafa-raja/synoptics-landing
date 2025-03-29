262aa2cf94236206bed2d9f7caff678166bf99e8
Mustafa-raja <95049133+Mustafa-raja@users.noreply.github.com>
2025-03-30 01:11:58 +0500

    rag-ideas

--- app/rag-application/page.tsx
+++ app/rag-application/page.tsx
@@ -1,12 +1,16 @@
+'use client'
+
 import RagHero from "./sections/hero";
 import Features from "./sections/features";
+import Ideas from "./sections/ideas";
 
 export default function RagApplication() {
     return (
-        <>
+        <main className="w-full">
             <RagHero />
             <Features />
-        </> 
+            <Ideas />
+        </main>
     );
 }
  
